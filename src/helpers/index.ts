f472e5f552452153e9851847f8b50346cec9ca84
Baimenovbn <baibn@mail.ru>
2021-10-16 17:20:01 +0600

    refactor: render more data on the map

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,16 +1,18 @@
-import mainClients from '../../data/main/NeRelog_clients.json';
+import mainClients from '../data/main/NeRelog_clients.json';
 import { Client } from '@/models/interfaces/client';
 import { Order, OrderView } from '@/models/interfaces/order';
-import mainOrders from '@/../data/main/NeRelog_apps.json'
+import mainOrders from '@/./data/main/NeRelog_apps.json';
+import additionalOrders from '@/./data/additional/NeRelog_apps.json';
+import additionalClients from '@/./data/additional/NeRelog_clients.json';
 
 export function getOrderViews(): OrderView[] {
   let clientNames = new Map<number, string>();
-  mainClients.forEach((client: Client) => {
+  additionalClients.forEach((client: Client) => {
     clientNames.set(client.id, client.name);
   })
 
-  return mainOrders.map((order: Order) => ({
+  return additionalOrders.map((order: Order) => ({
     ...order,
-    clientName: clientNames.get(order.client_id) || 'This order has no name'
+    clientName: clientNames.get(order.client_id) || 'This order has no client name'
   }))
 }
